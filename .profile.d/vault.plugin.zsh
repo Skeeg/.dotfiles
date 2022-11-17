90c79af548006a69bfba595dd8cfe3d125a7cc76
Skeeg <ryan-peay@pluralsight.com>
2022-11-16 22:06:23 -0700

    Extend to allow for returning more json data

--- .profile.d/vault.plugin.zsh
+++ .profile.d/vault.plugin.zsh
@@ -1,5 +1,11 @@
 vault-login() {
-  [[ -z $1 ]] && { echo 'usage: vault-login $VAULT_ADDRESS'; return 1;}
+  [[ -z $1 ]] && { 
+    echo 'Default usage specifically provides the token only from `.auth.client_token`'
+    echo 'usage: vault-login $VAULT_ADDRESS'
+    echo 'optionally, provide a json path to return more data for other users'; 
+    echo 'usage: vault-login $VAULT_ADDRESS .auth';
+    return 1;}
+  [[ ! -z $2 ]] && { JQ_PATH=$2 } || { JQ_PATH=".auth.client_token" }
   [[ -z "$OKTA_PASSWORD" || -z "$OKTA_USERNAME" ]] && { 
     echo "Initialize secrets first"; 
     echo 'secret export OKTA_USERNAME';
@@ -9,7 +15,7 @@ vault-login() {
     --request POST \
     --data "{\"password\": \"$OKTA_PASSWORD\"}" \
     $1/v1/auth/okta/login/$OKTA_USERNAME | \
-    jq -r .auth.client_token}
+    jq -r $JQ_PATH}
 }
 
 vp() {
