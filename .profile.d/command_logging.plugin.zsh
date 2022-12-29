eaec7786e3a51a8aaddf55fb04a9c1f02dfac727
Skeeg <ryan-peay@pluralsight.com>
2022-12-29 10:12:53 -0700

    zsh shell constrained

--- .profile.d/command_logging.plugin.zsh
+++ .profile.d/command_logging.plugin.zsh
@@ -1,17 +1,20 @@
-if (( ! $+_cmd_log_fd )); then
-  zmodload zsh/system
-  typeset -gi _cmd_log_fd
-  sysopen -a -m 600 -o cloexec,creat -u _cmd_log_fd ~/.zsh_cmdlog 2>/dev/null
-  (( $? )) && _cmd_log_fd=-1
-fi
+if [ $(echo $SHELL | rev | cut -d"/" -f1 | rev) = "zsh" ]; 
+then
+  if (( ! $+_cmd_log_fd )); then
+    zmodload zsh/system
+    typeset -gi _cmd_log_fd
+    sysopen -a -m 600 -o cloexec,creat -u _cmd_log_fd ~/.zsh_cmdlog 2>/dev/null
+    (( $? )) && _cmd_log_fd=-1
+  fi
 
-if (( _cmd_log_fd >= 0 )); then
-  function _cmd_log_preexec() {
-    emulate -L zsh
-    print -r -- "${(%):-%D{%Y-%m-%d %H:%M:%S %Z\}} $LOGNAME: $3" >&$_cmd_log_fd
-  }
-  autoload -Uz add-zsh-hook
-  add-zsh-hook preexec _cmd_log_preexec
-fi
+  if (( _cmd_log_fd >= 0 )); then
+    function _cmd_log_preexec() {
+      emulate -L zsh
+      print -r -- "${(%):-%D{%Y-%m-%d %H:%M:%S %Z\}} $LOGNAME: $3" >&$_cmd_log_fd
+    }
+    autoload -Uz add-zsh-hook
+    add-zsh-hook preexec _cmd_log_preexec
+  fi
 
-function cmdhistory() { cat ~/.zsh_cmdlog }
+  function cmdhistory() { cat ~/.zsh_cmdlog }
+fi
--- .profile.d/history.plugin.zsh
+++ .profile.d/history.plugin.zsh
@@ -1,6 +1,6 @@
 HISTSIZE=100000
 SAVEHIST=$HISTSIZE #zsh
-#HISTFILESIZE=$HISTSIZE #bash
+HISTFILESIZE=$HISTSIZE #bash
 HISTFILE="$HOME/.zsh_history"
 
 # Configure history stamp format
@@ -8,27 +8,29 @@ HIST_STAMPS="yyyy-mm-dd"
 HISTTIMEFORMAT="%d/%m/%y %T "
 
 export HISTSIZE SAVEHIST HISTFILESIZE HISTFILE HIST_STAMPS HISTTIMEFORMAT
-
-setopt extendedhistory
-setopt histexpiredupsfirst
-setopt histignoredups
-setopt histignorespace
-setopt histverify
-setopt sharehistory
-unsetopt noappendhistory
-unsetopt nobanghist
-unsetopt cshjunkiehistory
-unsetopt histallowclobber
-unsetopt nohistbeep
-unsetopt histfcntllock
-unsetopt histfindnodups
-unsetopt histignorealldups
-unsetopt histlexwords
-unsetopt histnofunctions
-unsetopt histnostore
-unsetopt histreduceblanks
-unsetopt nohistsavebycopy
-unsetopt histsavenodups
-unsetopt histsubstpattern
-unsetopt incappendhistory
-unsetopt incappendhistorytime
+if [ $(echo $SHELL | rev | cut -d"/" -f1 | rev) = "zsh" ]; 
+then
+  setopt extendedhistory
+  setopt histexpiredupsfirst
+  setopt histignoredups
+  setopt histignorespace
+  setopt histverify
+  setopt sharehistory
+  unsetopt noappendhistory
+  unsetopt nobanghist
+  unsetopt cshjunkiehistory
+  unsetopt histallowclobber
+  unsetopt nohistbeep
+  unsetopt histfcntllock
+  unsetopt histfindnodups
+  unsetopt histignorealldups
+  unsetopt histlexwords
+  unsetopt histnofunctions
+  unsetopt histnostore
+  unsetopt histreduceblanks
+  unsetopt nohistsavebycopy
+  unsetopt histsavenodups
+  unsetopt histsubstpattern
+  unsetopt incappendhistory
+  unsetopt incappendhistorytime
+fi
